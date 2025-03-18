7649e38912ae300befe9eac524ac724d005ab5be
Karol <karol.zakrzewski@outlook.com>
2025-03-18 16:25:12 +0100

    feat: add Gallery component with masonry layout and lightbox support, include new images

---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,9 @@
+import { Gallery } from "@/components/Gallery";
+
+export default function Page() {
+  return (
+    <div className="max-w-screen-xl mx-auto w-full">
+      <Gallery />
+    </div>
+  );
+}
