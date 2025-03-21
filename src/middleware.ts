f127ac2335ef685d6aa1e37425c7dafb4c1e9868
Karol <karol.zakrzewski@outlook.com>
2025-03-21 21:58:42 +0100

    feat: add middleware to handle request rewrites for specific routes

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.rewrite(new URL("/not-found", request.url));
+}
+
+export const config = {
+  matcher: ["/o-nas", "/kontakt", "/atrakcje", "/gastronomia"],
+};
