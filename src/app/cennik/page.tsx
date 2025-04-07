4ae22c322afbdf1883af55f52ed32d64774bb10c
Karol <karol.zakrzewski@outlook.com>
2025-04-07 19:47:57 +0200

    feat: add iframe to display cennik.html in the Cennik page

--- src/app/cennik/page.tsx
+++ src/app/cennik/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Page() {
-  return <div>Cennik</div>;
+  return (
+    <div>
+      <iframe
+        src="/cennik.html"
+        style={{ width: "100%", minHeight: "100vh", border: "none" }}
+        title="Cennik"
+      />
+    </div>
+  );
 }
