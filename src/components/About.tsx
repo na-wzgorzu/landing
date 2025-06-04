c832fb20eeb6271d4cb620d65ce44b90bd50f6e2
Karol <karol.zakrzewski@outlook.com>
2025-06-04 17:25:09 +0200

    feat: enhance About component with new content and image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
+const aboutContent = {
+  first:
+    "Ośrodek Wypoczynkowy „Na Wzgórzu” jest położony na terenie Brodnickiego Parku Krajobrazowego, nad jeziorem Strażym (73 ha), w odległości 12 km od miasta Brodnica, 60 km od Torunia i 200 km od Warszawy, na pograniczu województw Kujawsko-Pomorskiego oraz Warmińsko-Mazurskiego.",
+  second:
+    "Urozmaicony teren, czyste lasy i jeziora, rezerwaty przyrody, wiele malowniczych zakątków oraz brak większych miast i przemysłu sprawiają, że okolica stanowi idealne miejsce na wypoczynek, jednocześnie wciąż pozostając nieodkrytą przez masową turystykę.",
+};
+
 export const About = () => {
   return (
     <section>
@@ -16,12 +23,33 @@ const AboutHeader = () => {
       </div>
 
       <div className="flex gap-4 flex-col md:flex-row">
-        <AboutContent key="firstImg" img="/gallery/bridge.jpg" />
+        <AboutContent
+          key="firstImg"
+          img="/gallery/bridge.jpg"
+          content={aboutContent.first}
+        />
         <AboutContent
           key="SecondImg"
           img="/gallery/inne102.jpg"
-          classNames="md:flex-col-reverse"
+          classNames="md:flex-col-reverse justify-end"
+          content={aboutContent.second}
         />
+      </div>
+
+      <div>
+        <h3>
+          Jezioro Strażym, nad którym zlokalizowany jest ośrodek stanowi część
+          głównego szlaku wodnego łączącego całe Pojezierze Brodnickie.
+        </h3>
+        <div>
+          <Image
+            width={800}
+            height={800}
+            alt="About image"
+            src="/gallery/jezioro.jpg"
+            className="w-full"
+          />
+        </div>
       </div>
     </div>
   );
@@ -30,9 +58,11 @@ const AboutHeader = () => {
 const AboutContent = ({
   img,
   classNames,
+  content,
 }: {
   classNames?: string;
   img: string;
+  content: string;
 }) => {
   return (
     <div className={`flex flex-col gap-4 ${classNames}`}>
@@ -45,12 +75,7 @@ const AboutContent = ({
           className="w-full"
         />
       </div>
-      <div>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
-      </div>
+      <div>{content}</div>
     </div>
   );
 };
