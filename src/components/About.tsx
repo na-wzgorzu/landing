9ae685070963b348787c1fabcbf098e93c0a883c
Karol <karol.zakrzewski@outlook.com>
2025-06-08 12:05:30 +0200

    fix: enhance About component content and adjust layout for better readability

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const aboutContent = {
   first:
     "Ośrodek Wypoczynkowy „Na Wzgórzu” jest zlokalizowany na terenie Brodnickiego Parku Krajobrazowego, nad jeziorem Strażym, w odległości 12 km od miasta Brodnica, 60 km od Torunia i 200 km od Warszawy, na pograniczu województw Kujawsko-Pomorskiego oraz Warmińsko-Mazurskiego.",
   second:
-    "Urozmaicony teren, czyste lasy i jeziora, rezerwaty przyrody, wiele malowniczych zakątków oraz brak większych miast i przemysłu sprawiają, że okolica stanowi idealne miejsce na wypoczynek, jednocześnie wciąż pozostając nieodkrytą przez masową turystykę.",
+    "Urozmaicony teren, czyste lasy i jeziora, rezerwaty przyrody, wiele malowniczych zakątków oraz brak większych miast i przemysłu sprawiają, że okolica stanowi idealne miejsce na wypoczynek, jednocześnie wciąż pozostając nieodkrytą przez masową turystykę. Jezioro Strażym (73 ha) stanowi część głównego szlaku wodnego łączącego całe Pojezierze Brodnickie.",
 };
 
 export const About = () => {
@@ -17,7 +17,7 @@ export const About = () => {
 
 const AboutHeader = () => {
   return (
-    <div className="flex flex-col mx-auto max-w-screen-lg gap-2 px-4">
+    <div className="flex flex-col mx-auto max-w-screen-lg gap-8 px-4">
       <div className=" text-center underline text-gray-700 font-semibold font-mono text-3xl underline-offset-8 decoration-gray-700">
         Położenie
       </div>
@@ -36,21 +36,15 @@ const AboutHeader = () => {
         />
       </div>
 
-      <div>
-        <h3>
-          Jezioro Strażym (73 ha) stanowi część głównego szlaku wodnego
-          łączącego całe Pojezierze Brodnickie.
-        </h3>
-        <div className="mt-2">
-          <Image
-            width={1000}
-            height={1000}
-            quality={100}
-            alt="About image"
-            src="/gallery/jezioro.jpg"
-            className="w-full"
-          />
-        </div>
+      <div className="-mt-6">
+        <Image
+          width={1000}
+          height={1000}
+          quality={100}
+          alt="About image"
+          src="/gallery/jezioro.jpg"
+          className="w-full"
+        />
       </div>
     </div>
   );
