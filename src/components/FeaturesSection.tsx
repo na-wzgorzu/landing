ad54ea9964b3fa5ae09c943018fc320120d8df4e
Karol <karol.zakrzewski@outlook.com>
2025-06-16 08:59:22 +0200

    feat: add new images for About section and enhance Features and Story sections with improved content and layout

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -1,28 +1,45 @@
 import { Check } from "lucide-react";
 
+const resortFeatures: string[] = [
+  "Cichy, bezpieczny, kameralny i przyjazny rodzinom ośrodek",
+  "Lokalizacja w Brodnickim Parku Krajobrazowym",
+  "Plac zabaw dla dzieci, pokój zabaw, salę gier i inne atrakcje",
+  "Różne opcje zakwaterowania (domki, apartamenty, pokoje)",
+  "Pełne wyżywienie z domowymi posiłkami",
+  "Dostęp do plaży i możliwość wypożyczenia sprzętu wodnego",
+  "Wypożyczalnię rowerów górskich",
+  "Boisko do siatkówki, koszykówki, badmintona",
+  "Położenie nad jeziorem będącym częścią głównego szlaku wodnego pojezierza",
+  "Szlaki turystyczne, ścieżki rowerowe i trasy kajakowe, wszystkie z pięknymi widokami",
+  "Ogniska z kiełbaskami",
+  "Możliwość przyjazdu ze zwierzętami",
+  "Zadbany i zagospodarowany teren, altanki, bujawki, komplet wypoczynkowy, itp.",
+];
+
 export const FeaturesSection = () => {
   const features = [
     {
       title: "Cisza i spokój",
       description:
-        "Nasz ośrodek to zamknięty teren, gdzie można odpocząć od zgiełku miasta.",
+        "Nasz ośrodek jest kameralny, ma zamknięty teren, gdzie można odpocząć od zgiełku miasta.",
       delay: "0",
     },
     {
       title: "Bliskość natury",
       description:
-        "Otaczają nas rezerwaty przyrody, lasy i malownicze krajobrazy.",
+        "Otaczają nas rezerwaty przyrody, jeziora, lasy i malownicze krajobrazy.",
       delay: "200",
     },
     {
-      title: "Komfortowe zakwaterowanie",
+      title: "Zakwaterowanie",
       description:
-        "Domki, apartamenty z ogródkiem oraz pokoje w budynku głównym.",
+        "W domkach różnego typu (każdy z własnym, wydzielonym ogródkiem) oraz pokojach w budynku głównym.",
       delay: "400",
     },
     {
-      title: "Bogata infrastruktura rekreacyjna",
-      description: "Plaża, marina, boiska sportowe, plac zabaw i sala gier.",
+      title: "Infrastruktura rekreacyjna",
+      description:
+        "Plaża, marina, wypożyczalnie, boiska, plac zabaw i sala gier.",
       delay: "600",
     },
     {
@@ -31,13 +48,18 @@ export const FeaturesSection = () => {
         "Serwujemy pełne wyżywienie, także w wersjach wegetariańskich, wegańskich i bezglutenowych.",
       delay: "800",
     },
+    {
+      title: "Akceptujemy zwierzęta",
+      description: "Możecie Państwo przyjechać do nas ze swoimi pupilami.",
+      delay: "1000",
+    },
   ];
 
   return (
     <section className="py-16 md:py-24 bg-resort-beige">
       <div className="section-container">
         {/* Section header */}
-        <div className="text-center max-w-3xl mx-auto mb-16 space-y-4">
+        <div className="text-center max-w-3xl mx-auto mb-8 space-y-4 px-4">
           <div className="mx-auto w-16 h-[2px] bg-resort-green  fade-in"></div>
           <h2 className="text-3xl sm:text-4xl font-medium text-resort-text  fade-in animation-delay-200">
             Dlaczego warto nas wybrać?
@@ -54,7 +76,7 @@ export const FeaturesSection = () => {
           {features.map((feature, index) => (
             <div
               key={index}
-              className="feature-card "
+              className="feature-card"
               style={{ animationDelay: `${parseInt(feature.delay) + 600}ms` }}
             >
               <div className="flex items-start">
@@ -78,23 +100,13 @@ export const FeaturesSection = () => {
             Co oferujemy
           </h3>
 
-          <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 stylized-list">
-            <li>Cichy, bezpieczny i przyjazny rodzinom ośrodek</li>
-            <li>
-              Położenie w Brodnickim Parku Krajobrazowym, otoczonym przyrodą
-            </li>
-            <li>Komfortowe zakwaterowanie (domki, apartamenty, pokoje)</li>
-            <li>Pełne wyżywienie z domowymi posiłkami</li>
-            <li>Dostęp do prywatnej plaży z wygodnymi leżakami</li>
-            <li>Marina z wypożyczalnią łodzi i kajaków</li>
-            <li>Boiska sportowe do tenisa, siatkówki i koszykówki</li>
-            <li>
-              Plac zabaw dla dzieci z nowoczesnym, bezpiecznym wyposażeniem
-            </li>
-            <li>
-              Szlaki turystyczne i ścieżki rowerowe z malowniczymi widokami
-            </li>
-            <li>Sezonowe wycieczki z przewodnikiem i zajęcia przyrodnicze</li>
+          <ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            {resortFeatures.map((feature, index) => (
+              <li key={index} className="flex gap-2">
+                <div className="w-2 h-2 rounded-full bg-brand mt-2" />
+                <div className="text-gray-700 w-full">{feature}</div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- src/components/StorySection.tsx
+++ src/components/StorySection.tsx
@@ -2,12 +2,12 @@ import { MoveRight } from "lucide-react";
 
 export const StorySection = () => {
   return (
-    <section className="py-16 md:py-24">
+    <section className="py-16 md:py-24 px-4">
       <div className="max-w-screen-xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
         {/* Left column: Image */}
         <div className=" fade-in">
           <img
-            src="/gallery/kayak.jpg"
+            src="/about/domek.jpg"
             alt="Rzeka pomiędzy górami w Brodnickim Parku Krajobrazowym"
             className="w-full h-[400px] sm:h-[500px] object-cover rounded-xl"
             loading="lazy"
@@ -22,31 +22,29 @@ export const StorySection = () => {
           </div>
 
           <h3 className="text-3xl sm:text-4xl font-medium text-resort-text  fade-in animation-delay-200">
-            Rodzinny wypoczynek w otoczeniu natury
+            Rodzinny wypoczynek w otoczeniu przyrody
           </h3>
 
           <div className="space-y-4  fade-in animation-delay-400">
             <p className="text-resort-text/80 leading-relaxed">
-              Ośrodek Wypoczynkowy Na Wzgórzu to miejsce stworzone z myślą o
-              rodzinach, które szukają spokoju, bliskości natury i komfortowego
-              wypoczynku. Położony na terenie malowniczego Brodnickiego Parku
-              Krajobrazowego, nad czystym i urokliwym Jezioro Strażym, stanowi
-              idealne miejsce na relaks z dala od miejskiego zgiełku.
+              Ośrodek Wypoczynkowy Na Wzgórzu to miejsce stworzone dla osób
+              szukających spokoju, bliskości natury i wypoczynku w domowej
+              atmosferze. Obiekt jest kameralny, położony na terenie
+              malowniczego Brodnickiego Parku Krajobrazowego, w otoczeniu
+              rezerwatów przyrody, nad urokliwym Jeziorem Strażym. Idealnie
+              nadaje się na relaks z dala od miejskiego zgiełku oraz natłoku
+              turystów.
             </p>
             <p className="text-resort-text/80 leading-relaxed">
-              Od lat przyjmujemy rodziny z dziećmi oraz dziadków z wnukami,
-              dbając o domową atmosferę, bezpieczeństwo i niezapomniane
-              wspomnienia naszych gości.
+              Naszymi klientami są głównie rodziny z dziećmi oraz dziadkowie z
+              wnukami. Teren obiektu jest ogrodzony i bezpieczny. Nie
+              przyjmujemy kolonii, a w okresie wakacyjnym również innych grup
+              zorganizowanych, nie ma tu wesel ani imprez, jest za to cicho.
             </p>
           </div>
 
-          <div className="pt-2  fade-in animation-delay-600">
-            <a
-              href="#values"
-              className="inline-flex items-center text-resort-green hover:underline font-medium"
-            >
-              Poznaj nasze wartości <MoveRight size={16} className="ml-1" />
-            </a>
+          <div className="fade-in animation-delay-600 inline-flex items-center text-resort-green hover:underline font-medium">
+            Poznaj nasze wartości <MoveRight size={16} className="ml-1" />
           </div>
         </div>
       </div>
