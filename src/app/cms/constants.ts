8222c4b64214cedf6f822eea065f12026194eeff
Karol <karol.zakrzewski@outlook.com>
2026-05-05 09:37:23 +0200

    feat: update reservation submission to use constant for API URL

--- src/app/cms/constants.ts
+++ src/app/cms/constants.ts
@@ -2,3 +2,4 @@ export const SESSION_KEY = "cms_auth";
 export const API_URL = "https://na-wzgorzu.pl/api/api.php";
 export const IMAGES_URL = "https://na-wzgorzu.pl/api/images.php";
 export const AUTH_URL = "https://na-wzgorzu.pl/api/auth.php";
+export const RESERVATION_URL = "https://na-wzgorzu.pl/api/reservation.php";
